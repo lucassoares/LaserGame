bce4f476740baa42472d24d07feae2a2609060ba
Lucas Soares <lsoares267@gmail.com>
2017-11-22 09:34:51 -0200

    Add files via upload

---
+++ testeLdr.ino
@@ -0,0 +1,16 @@
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  Serial.print("LDR1 ");
+  Serial.print(analogRead(A0));
+  Serial.print(" LDR2 ");
+  Serial.print(analogRead(A1));
+  Serial.print(" LDR3 ");
+  Serial.print(analogRead(A2));
+  Serial.print(" LDR4 ");
+  Serial.println(analogRead(A3));
+}
